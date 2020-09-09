51a6cc308411505bc0cae8b693b2091c786e3db6
sforaaleksander <sforaaleksander@gmail.com>
2020-09-09 12:05:04 +0200

    implement sendResponse() in HttpRequestHandler.java

--- src/main/java/com/codecool/summer/App.java
+++ src/main/java/com/codecool/summer/App.java
@@ -1,8 +1,11 @@
 package com.codecool.summer;
 
+
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Summer summer = new Summer();
         summer.registerHandlers(ExampleHandler.class);
         summer.run();
--- src/main/java/com/codecool/summer/HttpRequestHandler.java
+++ src/main/java/com/codecool/summer/HttpRequestHandler.java
@@ -1,13 +1,14 @@
 package com.codecool.summer;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.Map;
 
-public class HttpRequestHandler {
+public class HttpRequestHandler implements HttpHandler {
     final Map<String, Method> handlerMethods;
 
     public HttpRequestHandler(Map<String, Method> handlerMethods) {
@@ -18,13 +19,15 @@ public class HttpRequestHandler {
      * Invokes proper method handling proper endpoint and sends HTTP Response back.
      * @param httpExchange Encapsulated HTTP request.
      */
-    public void handle(HttpExchange httpExchange) {
-        // TODO
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
         sendResponse(httpExchange, 500, "Not Implemented");
     }
 
-    private void sendResponse(HttpExchange httpExchange, int httpStatusCode, String message) {
-        // TODO
+    private void sendResponse(HttpExchange httpExchange, int httpStatusCode, String message) throws IOException {
+        httpExchange.sendResponseHeaders(200, message.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(message.getBytes());
+        os.close();
     }
-
 }
