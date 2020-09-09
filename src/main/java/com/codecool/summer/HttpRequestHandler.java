cfb7d9bbdb94089256a304dbab477033cade721b
sforaaleksander <sforaaleksander@gmail.com>
2020-09-09 12:36:16 +0200

    implement handle() in HttpRequestHandler.java

--- src/main/java/com/codecool/summer/HttpRequestHandler.java
+++ src/main/java/com/codecool/summer/HttpRequestHandler.java
@@ -5,6 +5,7 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
 
@@ -21,11 +22,26 @@ public class HttpRequestHandler implements HttpHandler {
      */
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        sendResponse(httpExchange, 500, "Not Implemented");
+        String uri = httpExchange.getRequestURI().toString();
+        String response = "";
+        int httpStatusCode;
+        if (!handlerMethods.containsKey(uri)){
+            sendResponse(httpExchange, 404, "wrong path provided");
+            return;
+        }
+        try {
+            response = (String) handlerMethods.get(uri).invoke(null);
+            httpStatusCode = 200;
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            response = "internal server error";
+            httpStatusCode = 500;
+            e.printStackTrace();
+        }
+        sendResponse(httpExchange, httpStatusCode, response);
     }
 
     private void sendResponse(HttpExchange httpExchange, int httpStatusCode, String message) throws IOException {
-        httpExchange.sendResponseHeaders(200, message.length());
+        httpExchange.sendResponseHeaders(httpStatusCode, message.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(message.getBytes());
         os.close();
